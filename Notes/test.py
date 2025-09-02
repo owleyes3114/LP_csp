3b5d37c0d492d225073d95caa107e00f865c3c64
owleyes3114 <leila.palmer@ucas-edu.net>
2025-09-02 13:44:44 -0600

    program variuables practice

--- Notes/test.py
+++ Notes/test.py
@@ -1,3 +1,14 @@
 #LP 6 GitHub Test
 
 print("Hi")
+name="UCAS"
+year=2025
+user=input("What is your name?\n")
+print(user)
+print(year)
+print(609)
+breakfast="nothing" 
+color="teal"
+eye="Hazel"
+age="14"
+subject=input("what is your favorte school subject?\n")
