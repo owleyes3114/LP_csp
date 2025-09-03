d73e3581471c0c456faf83520465d1ce6b109d74
owleyes3114 <leila.palmer@ucas-edu.net>
2025-09-03 13:44:55 -0600

    variable

--- Notes/test.py
+++ Notes/test.py
@@ -2,13 +2,20 @@
 
 print("Hi")
 name="UCAS"
+print("UCAS")
 year=2025
+print(year)
 user=input("What is your name?\n")
 print(user)
-print(year)
-print(609)
-breakfast="nothing" 
+
+number=908
+print(908)
+breakfast="nothing"
+print(nothing)
 color="teal"
+print(color)
 eye="Hazel"
+print(eye)
 age="14"
+print(age)
 subject=input("what is your favorte school subject?\n")
