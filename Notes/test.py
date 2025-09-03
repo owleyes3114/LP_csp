7f47d67d878bcac7caabf538b6bcea1eeb0d298e
owleyes3114 <leila.palmer@ucas-edu.net>
2025-09-03 19:44:20 +0000

    Variable edits pt2

--- Notes/test.py
+++ Notes/test.py
@@ -1,14 +1,24 @@
 #LP 6 GitHub Test
 
 print("Hi")
-name="UCAS"
+
+school="UCAS"
+
 year=2025
+
 user=input("What is your name?\n")
-print(user)
+
 print(year)
 print(609)
+
 breakfast="nothing" 
+print(breakfast)
+
 color="teal"
+print(color)
+
 eye="Hazel"
+print(eye)
+
 age="14"
 subject=input("what is your favorte school subject?\n")
