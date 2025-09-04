183760463246bfda20ef960ad084734616e884bf
owleyes3114 <leila.palmer@ucas-edu.net>
2025-09-04 13:43:24 -0600

    Expressions Notes

---
+++ Notes/notes.py
@@ -0,0 +1,49 @@
+# LP 6th Expressions Notes!
+
+# What is an algorithim?
+    # A set of intructions to accoplish a task
+
+name=input("What is your name?")
+print("Hello", name)
+
+# algorithim for area 
+length=5
+width=20
+area=length * width
+print(length, "*", width, "=", area)
+
+# List steps in an algorithm
+    # 1. What varables we need/pieces of infortmation needed
+    # 2. Process info
+    # 3. Provide output
+
+person1 = 14
+person2 = 14
+person3 = 14
+person4 = 14
+
+average=(person1 + person2 + person3 + person4)/4
+print(average)
+
+print("The average age being:", person1, "-", person2, "-", person3, "-", person4, "-", average)
+
+# Math equations!
+num_1 = 12
+num_2 = 4
+
+print("addition:", num_1 + num_2)
+print("subtraction:", num_1 - num_2)
+print("Multipilcation:", num_1 * num_2)
+print("Division:", num_1 / num_2)
+print("exponents:", num_1 ** num_2)
+print("integer diviion:", num_1 // num_2)
+print("modulo (remainder):", num_1 % num_2)
+
+# an expression...
+   # is a collection of numbers and symbols imn order to get a restult. 
+
+# List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+    # add=+
+    # multiply=*
+    # subtract=-
+    # divide=/
