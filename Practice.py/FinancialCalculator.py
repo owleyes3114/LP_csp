90157922439134b2f26b154c7c9b2e668be17ac6
owleyes3114 <leila.palmer@ucas-edu.net>
2025-09-08 12:51:06 -0600

    string notes

---
+++ Practice.py/FinancialCalculator.py
@@ -0,0 +1,13 @@
+# LP 6th 
+
+# income 3000 =percent of imncome
+# rent; 1200
+#Utilites: 250
+#Groceries: 200
+#Trnsportaion: 500
+
+#saving should be 300
+# percet_rent: 1200/3000
+etc. 
+#tell= total expensives, saving, and spening 
+add all up
