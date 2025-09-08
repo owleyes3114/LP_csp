0a8c1897476096820712b4a6d0e0fcd418472190
owleyes3114 <leila.palmer@ucas-edu.net>
2025-09-08 13:44:51 -0600

    practice

--- Practice.py/stringnotes.py
+++ Practice.py/stringnotes.py
@@ -1,3 +1,46 @@
 # LP 6th String Notes
 
 print("I did it")
+
+#-Octothorp
+
+#What makes something a string?
+    #  A collection of symbols held together by quetation marks. The only data type that lets ,us use letters ("") and (''). Doed not matter as long as the qutations are matching.  
+
+    #examples: 
+first_name=input("What is your first name:\n").title()
+last_name=input("What is your first name:\n").title()
+full_name = first_name + " " + last_name
+
+print("welcome")
+
+# strip=gets rid of any uneccesary spaces at begining or end of a string
+#.upper=turns reponse into capital letters
+#.lower=make respone lowercase
+#the last command at the end of the string is what the prgram will go with
+
+scentence= "The quick brown fox jumps oiver the lazy dog."
+
+print("Welcome to my program,", name, "!")
+
+#cacatination 
+
+#Why do we have strings?
+    #
+
+# How do stupid proofing and sanitization relate to each other?
+
+
+#What is debugging?
+
+
+#How do you debug the different types of errors?
+
+
+#Describe what each of the methods below does:
+
+#find()
+#concatenate (add)
+#upper()
+#lower()
+#strip()
